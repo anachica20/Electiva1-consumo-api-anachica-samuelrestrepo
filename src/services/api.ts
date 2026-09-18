300df1d0c6e87432d8e438dcb8766b54dc093002
Ana Chica <177176570+anachica20@users.noreply.github.com>
2026-09-18 18:43:04 -0500

    feat: agregar consulta de personaje por id

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-import type { ResponseCharacter } from '../types/api'
+import type { ResponseCharacter, Character } from '../types/api'
 
 export const getCharacters = async (): Promise<ResponseCharacter> => {
   const baseUrl = 'https://rickandmortyapi.com/api/character'
@@ -10,6 +10,20 @@ export const getCharacters = async (): Promise<ResponseCharacter> => {
   }
 
   const data = (await response.json()) as ResponseCharacter
+
+  return data
+}
+
+export const getCharacterById = async (id: number): Promise<Character> => {
+  const baseUrl = `https://rickandmortyapi.com/api/character/${id}`
+
+  const response = await fetch(baseUrl)
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch character: ${response.status}`)
+  }
+
+  const data = (await response.json()) as Character
 
   return data
 }
